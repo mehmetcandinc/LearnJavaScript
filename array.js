a07bb6a5f99c5803b4e8bb9a4d4b5780dddd9610
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-15 14:38:20 +0300

    array.js dosyasına array örneği yazıldı

---
+++ array.js
@@ -0,0 +1,8 @@
+/*array ve array'e eleman ekleme,,, arrayadi[sonindex+1]="eklenicek şey"
+    ama asıl arrayadi.push("eklenilicek şey") şeklinde ekleme yapmak.*/
+
+    let array = ["Java","C","C++","Python"];
+    array.push("PHP");
+    alert(array);
+    /*dizi içerisindeki eleman sayısı
+    alert(array.length);*/
