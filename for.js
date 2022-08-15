514dbbc736a80adb7d5afd59a5def672102029b5
mehmetcandinc <dincmehmetcann@gmail.com>
2022-08-15 15:02:15 +0300

    forEach.js oluşturuldu. for.js içinde bulunan forEach örneği taşındı

--- for.js
+++ for.js
@@ -4,11 +4,3 @@
     for(let i=0 ; i<sayilar.length ;i++ ){
         console.log("Eleman: " + sayilar[i]);
     }
-/*elemanların üzerinde tek tek gezin için sayilar2.forEach(function(sayi){})
-böylelikle for döngüsü kullanmamış olduk*/
-
-    let sayilar2 =[44,56,2,7,9,5]
-        console.log("Elemanlar. . .");
-        sayilar2.forEach(function(sayi){
-                console.log("Eleman: "+sayi);
-        })
---
+++ forEach.js
@@ -0,0 +1,8 @@
+/*elemanların üzerinde tek tek gezin için sayilar2.forEach(function(sayi){})
+böylelikle for döngüsü kullanmamış olduk*/
+
+let sayilar2 =[44,56,2,7,9,5]
+console.log("Elemanlar. . .");
+sayilar2.forEach(function(sayi){
+        console.log("Eleman: "+sayi);
+})
